302940e2d284c18ed81ec1456816a1b3006008d4
LaurePiechaczyk <laure.piecha@gmail.com>
2022-03-30 08:53:48 +0000

    small project ready for deployment

--- multi-language/src/App.js
+++ multi-language/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Contenu from "./Components/Contenu/Contenu";
+import ToggleLangs from "./Components/ToggleLangs/ToggleLangs";
+import ContextProvider from "./context/langContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <ContextProvider>
+      <ToggleLangs />
+      <Contenu />
+    </ContextProvider>
   );
 }
 
